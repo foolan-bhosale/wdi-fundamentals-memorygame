8e9a3c75c440261bfa6cb1bea6c8db6e64b4eb36
Foolan <foolanbhosale5@gmail.com>
2020-03-04 22:05:18 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,22 +2,26 @@ let cards = ["queen", "queen", "king", "king"];
 let cardsInPlay = [];
 let cardOne = cards[0];
 let cardTwo = cards[2];
-//console.log(cardOne);
+ 
 
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-
-console.log("user flipped" + " " + cardOne);
-
-console.log("user flipped"+" " + cardTwo);
-
-
-if (cardsInPlay.length === 2){
-  if (cardsInPlay[0] === cardsInPlay[1]) {
-    alert("You found a match!");
-  } else {
-    alert("Sorry, try again.")
+  function checkForMatch(){
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+      console.log("You found a match!");
+    } else {
+      console.log("Sorry, try again.")
+    }
   }
-} 
+
+function flipCard(cardId) {
+  console.log("User flipped " + cards[cardId]);
+  cardsInPlay.push(cards[cardId]);
+  if (cardsInPlay.length === 2){
+    checkForMatch();
+  } 
+}
+
+flipCard(0);
+flipCard(2);
+
+
 
